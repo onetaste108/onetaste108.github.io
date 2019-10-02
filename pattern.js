aa90ad7932fa76e23964bb0e7b19f052359d388a
unknown <onetaste108@gmail.com>
2019-10-02 14:03:56 +0300

    a

--- pattern.js
+++ pattern.js
@@ -180,6 +180,39 @@ function run_pattern(canvasId) {
   vx = 0;
   vy = 0;
 
+  canvas.width = canvas.clientWidth
+  canvas.height = canvas.clientHeight
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+
+
+
+  var gn = new GyroNorm();
+
+  gn.init().then(function(){
+    gn.start(function(data){
+      // Process:
+      // data.do.alpha	( deviceorientation event alpha value )
+      // data.do.beta		( deviceorientation event beta value )
+      // data.do.gamma	( deviceorientation event gamma value )
+      // data.do.absolute	( deviceorientation event absolute value )
+
+      // data.dm.x		( devicemotion event acceleration x value )
+      // data.dm.y		( devicemotion event acceleration y value )
+      // data.dm.z		( devicemotion event acceleration z value )
+
+      // data.dm.gx		( devicemotion event accelerationIncludingGravity x value )
+      // data.dm.gy		( devicemotion event accelerationIncludingGravity y value )
+      // data.dm.gz		( devicemotion event accelerationIncludingGravity z value )
+
+      // data.dm.alpha	( devicemotion event rotationRate alpha value )
+      // data.dm.beta		( devicemotion event rotationRate beta value )
+      // data.dm.gamma	( devicemotion event rotationRate gamma value )
+    });
+  }).catch(function(e){
+    alert("ERROR");
+  });
+
   function render() {
     update_input();
     mstep = 0.005;
